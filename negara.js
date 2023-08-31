d025266e5af374b3aa7f74324f490a3576b70572
felixleandr <felixleander2108@gmail.com>
2023-08-31 13:08:21 +0700

    halaman username

---
+++ negara.js
@@ -0,0 +1,124 @@
+let negara = [
+  {
+    nama: 'bhutan',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'andorra',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'bahrain',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'brunei',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'estonia',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'honduras',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'hungaria',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'jepang',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'kamerun',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'kroasia',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'lebanon',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'moldova',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'myanmar',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'nepal',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'norwegia',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'panama',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'prancis',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'rwanda',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'samoa',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'senegal',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'suriah',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'somalia',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'swedia',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'taiwan',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'thailand',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'tunisia',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'uganda',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'yunani',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'yordania',
+    hint: 'Terletak di Asia Selatan'
+  },
+  {
+    nama: 'zambia',
+    hint: 'Terletak di Asia Selatan'
+  },
+]
+
+console.log(negara.length);
